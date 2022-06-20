7a34eaf2aa0afeed5a4f63ca38ba908d49d2dd7f
Brave Okafor <okaforbrave@gmail.com>
2022-06-20 17:34:02 +0200

    Push

--- aks/storage.tf
+++ aks/storage.tf
@@ -1,4 +1,6 @@
-resource "azurerm_storage_account" "storage-account" {
+# UNCOMMENT IF YOU INTEND TO USE REMOTE STATE
+
+/* resource "azurerm_storage_account" "storage-account" {
   name                            = var.storage-account-name
   resource_group_name             = azurerm_resource_group.resource-group.name
   location                        = azurerm_resource_group.resource-group.location
@@ -11,10 +13,11 @@ resource "azurerm_storage_account" "storage-account" {
     maintainer  = var.maintainer
     location    = var.resource-group-location
   }
-}
+} */
 
-resource "azurerm_storage_container" "tfstate-storage-container" {
+/* resource "azurerm_storage_container" "tfstate-storage-container" {
   name                  = var.tfstate-storage-container-name
   storage_account_name  = azurerm_storage_account.storage-account.name
   container_access_type = "blob"
 }
+ */
--- aks/terraform.tfvars
+++ aks/terraform.tfvars
@@ -6,16 +6,15 @@ maintainer  = "Brave Okafor <okaforbrave@gmail.com>"
 resource-group-name     = "test-aks-rg"
 resource-group-location = "Central US" #"Germany West Central" # "West Europe"
 
-# STORAGE ACCOUNT
+# UNCOMMENT IF YOU INTEND TO USE REMOTE STATE
+/* # STORAGE ACCOUNT
 storage-account-name             = "darkeyetestakssa" #"deepeyesa"
 storage-account-tier             = "Standard"
 storage-account-replication-type = "LRS"
 
 # TFSTATE
-tfstate-storage-container-name = "tfstate"
+tfstate-storage-container-name = "tfstate" */
 
 # SERVICE PRINCIPAL
 aks_service_principal_app_id        = "<service_principal_app_id>"
 aks_service_principal_client_secret = "<service_principal_password>"
-
-#aks_service_principal_object_id = "<service_principal_object_id>"
